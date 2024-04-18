366b9319e7012a7dfe4d4c725c617a5f4d5aada5
Mendez <jose.mendez@sjcd.edu>
2024-04-18 05:42:37 -0500

    testing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@
  * See LICENSE in the project root for license information.
  */
 
-import { Client } from "@microsoft/microsoft-graph-client";
-import { PublicClientApplication, InteractionType } from "@azure/msal-browser";
-import { AuthCodeMSALBrowserAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser";
-import { LogLevel } from "@azure/msal-browser";
-
 /* global Office */
 
 class OfficeAuthProvider {
@@ -20,72 +15,12 @@ class OfficeAuthProvider {
   }
 }
 //const authProvider = new OfficeAuthProvider();
-Office.onReady(async (info) => {
-  const msalConfig = {
-    auth: {
-      clientId: "3773bcb1-8506-4ed6-92ef-ba4ecd0a31c4",
-      authority: "https://login.microsoftonline.com/514efd40-8efe-4f15-819f-34e56acf1562",
-      redirectUri: "https://localhost:3000/index.html",
-    },
-    cache: {
-      cacheLocation: "sessionStorage",
-      temporaryCacheLocation: "sessionStorage",
-      storeAuthStateInCookie: true,
-      secureCookies: false,
-      claimsBasedCachingEnabled: true,
-    },
-    system: {
-      loggerOptions: {
-        loggerCallback: (level, message, containsPii) => {
-          if (containsPii) {
-            return;
-          }
-          switch (level) {
-            case LogLevel.Error:
-              console.error(message);
-              return;
-            case LogLevel.Info:
-              console.info(message);
-              return;
-            case LogLevel.Verbose:
-              console.debug(message);
-              return;
-            case LogLevel.Warning:
-              console.warn(message);
-              return;
-          }
-        },
-        piiLoggingEnabled: false,
-      },
-      windowHashTimeout: 60000,
-      iframeHashTimeout: 10000,
-      loadFrameTimeout: 0,
-      asyncPopups: false,
-    },
-  };
-  const msalInstance = new PublicClientApplication(msalConfig);
-  this.msalInstance
-    .ssoSilent({
-      scopes: ["User.Read", "Mail.ReadWrite.Shared"],
-    })
-    .then((result) => {
-      this.msalInstance.setActiveAccount(result.account);
-      console.log(result.account);
-    });
-  const authProvider = new AuthCodeMSALBrowserAuthenticationProvider(msalInstance, {
-    account: this.msalInstance.getActiveAccount(),
-    InteractionType: InteractionType.Popup,
-    scopes: ["User.Read", "Mail.ReadWrite.Shared"],
-  });
-
-  const client = Client.initWithMiddleware({
-    authProvider: authProvider,
-  });
-  Office.context.roamingSettings.set("MSGraphClient", client);
-});
 
 async function claimEmail(event) {
   const client = Office.context.roamingSettings.get("MSGraphClient");
+  if (client === null){
+    Office.addin.showAsTaskpane();
+  } 
   console.log("Claiming Email...");
   //Get currently selected message reference
   const message = Office.context.mailbox.item;
