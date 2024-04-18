97eb8c468ee9f78726db2e7817223d2435a3fe3c
Mendez <jose.mendez@sjcd.edu>
2024-04-18 05:25:53 -0500

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@
 import { Client } from "@microsoft/microsoft-graph-client";
 import { PublicClientApplication, InteractionType } from "@azure/msal-browser";
 import { AuthCodeMSALBrowserAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/authCodeMsalBrowser";
-import msalConfig from "./authConfig";
+import { LogLevel } from "@azure/msal-browser";
+
 /* global Office */
 
 class OfficeAuthProvider {
@@ -20,6 +21,48 @@ class OfficeAuthProvider {
 }
 //const authProvider = new OfficeAuthProvider();
 Office.onReady(async (info) => {
+  const msalConfig = {
+    auth: {
+      clientId: "3773bcb1-8506-4ed6-92ef-ba4ecd0a31c4",
+      authority: "https://login.microsoftonline.com/514efd40-8efe-4f15-819f-34e56acf1562",
+      redirectUri: "https://localhost:3000/index.html",
+    },
+    cache: {
+      cacheLocation: "sessionStorage",
+      temporaryCacheLocation: "sessionStorage",
+      storeAuthStateInCookie: true,
+      secureCookies: false,
+      claimsBasedCachingEnabled: true,
+    },
+    system: {
+      loggerOptions: {
+        loggerCallback: (level, message, containsPii) => {
+          if (containsPii) {
+            return;
+          }
+          switch (level) {
+            case LogLevel.Error:
+              console.error(message);
+              return;
+            case LogLevel.Info:
+              console.info(message);
+              return;
+            case LogLevel.Verbose:
+              console.debug(message);
+              return;
+            case LogLevel.Warning:
+              console.warn(message);
+              return;
+          }
+        },
+        piiLoggingEnabled: false,
+      },
+      windowHashTimeout: 60000,
+      iframeHashTimeout: 10000,
+      loadFrameTimeout: 0,
+      asyncPopups: false,
+    },
+  };
   const msalInstance = new PublicClientApplication(msalConfig);
   this.msalInstance
     .ssoSilent({
